669bae3cb10f52f2a219b1e36a7253163dd606b3
Daniele-Simonetti <daniele.simonetti99@outlook.it>
2021-11-24 16:05:32 +0100

    end mail ex

--- mail/js/script.js
+++ mail/js/script.js
@@ -3,10 +3,24 @@
 // controlla che sia nella lista di chi può accedere,
 // stampa un messaggio appropriato sull’esito del controllo.
 
-// 1.creo un array con le mail verificate
 
-// 2.chiedo all'utente di inserire la sua mail
+// 1.chiedo all'utente di inserire la sua mail
+const mail = prompt('Inserisci la tua mail');
+// 2.creo un array con le mail verificate
+const MailsVerificate = ['mail 1', 'mail 2', 'mail 3'];
+// 3.inserisco la variabile true/false per non far cercare ogni singolo elemento del ciclo
+let find = false;
+// 4.verifico che la mail inserita dall'utante sia nell'array
+for (let i = 0; i < MailsVerificate.length; i++) {
+  if (mail.toLowerCase() == MailsVerificate[i].toLowerCase()) {
+    find = true;
+  } 
+}
 
-// 3.verifico che la mail inserita dall'utante sia nell'array
-
-// 4.se la mail è nell'elenco allora stamperò "mail verificata", altrimenti stamperò "mail non verificata"
+// 5.se la mail è nell'elenco allora stamperò "mail verificata", altrimenti stamperò "mail non verificata"
+console.log(find);
+if (find == true) {
+  console.log('Mail Verificata');
+} else {
+  console.log('Mail Non Verificata');
+}
