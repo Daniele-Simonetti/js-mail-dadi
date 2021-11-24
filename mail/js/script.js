62e0b2e3d0ce74122e9b0802e209a46e1f8d5655
Daniele-Simonetti <daniele.simonetti99@outlook.it>
2021-11-24 16:44:10 +0100

    testing with input

--- mail/js/script.js
+++ mail/js/script.js
@@ -5,18 +5,17 @@
 
 
 // 1.chiedo all'utente di inserire la sua mail
-const mail = prompt('Inserisci la tua mail');
+const Mail = prompt('Inserisci la tua mail');
 // 2.creo un array con le mail verificate
 const MailsVerificate = ['mail 1', 'mail 2', 'mail 3'];
 // 3.inserisco la variabile true/false per non far cercare ogni singolo elemento del ciclo
 let find = false;
 // 4.verifico che la mail inserita dall'utante sia nell'array
 for (let i = 0; i < MailsVerificate.length; i++) {
-  if (mail.toLowerCase() == MailsVerificate[i].toLowerCase()) {
+  if (Mail.toLowerCase() == MailsVerificate[i].toLowerCase()) {
     find = true;
   } 
 }
-
 // 5.se la mail è nell'elenco allora stamperò "mail verificata", altrimenti stamperò "mail non verificata"
 console.log(find);
 if (find == true) {
@@ -24,3 +23,11 @@ if (find == true) {
 } else {
   console.log('Mail Non Verificata');
 }
+
+
+// const button = document.getElementById('verifica');
+// button.addEventListener ('click', function() {
+//   const input = document.getElementById('Email');
+//   const Email = input.value;
+//   console.log(MailsVerificate[Email]);
+// });
