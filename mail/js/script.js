8d72122c819b1e19c317463ed1e5a218ffce801a
Daniele-Simonetti <daniele.simonetti99@outlook.it>
2021-11-24 15:22:02 +0100

    add logic structure

---
+++ mail/js/script.js
@@ -0,0 +1,12 @@
+// Mail
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+
+// 1.creo un array con le mail verificate
+
+// 2.chiedo all'utente di inserire la sua mail
+
+// 3.verifico che la mail inserita dall'utante sia nell'array
+
+// 4.se la mail è nell'elenco allora stamperò "mail verificata", altrimenti stamperò "mail non verificata"
