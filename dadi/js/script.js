8bcf322ed21d94bdf3704882b6b271d5d38e1adc
Daniele-Simonetti <daniele.simonetti99@outlook.it>
2021-11-24 16:54:54 +0100

    end

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -4,16 +4,16 @@
 
 
 // 1.genero un numero random da 1 a 6 per il giocatore
-const PlayerNumber = Math.floor(Math.random() * 6) + 1;
+let PlayerNumber = Math.floor(Math.random() * 6) + 1;
 console.log('Numero del giocatore', PlayerNumber);
 // 2.genero un numero random da 1 a 6 per il computer
-const ComputerNumber = Math.floor(Math.random() * 6) + 1;
+let ComputerNumber = Math.floor(Math.random() * 6) + 1;
 console.log('Numero del computer', ComputerNumber);
-// 3.se il dadi del pc è più malto stampo in console hai perso altrimenti hai vinto
+// 3.se il dado del pc è più malto stampo in console hai perso altrimenti hai vinto oppure pareggio
 if (PlayerNumber > ComputerNumber) {
-  // alert('Hai vinto');
   console.log('Hai vinto');
-} else {
-  // alert('Hai perso');
+} else if (PlayerNumber < ComputerNumber) {
   console.log('Hai perso');
+} else {
+  console.log('Pareggio');
 }
